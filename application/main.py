08ae178eb337963829d0e81dfac856de2e858696
iorjunior <juniorcoda@gmail.com>
2022-01-28 17:58:20 -0300

    Change de /

--- application/main.py
+++ application/main.py
@@ -5,26 +5,49 @@ main = Blueprint('main', __name__)
 
 
 @main.route("/")
-def get_trending():
-    since = request.args.get('since') if request.args.get(
-        'since') else 'daily'
+def main_page():
+    return '''
+    <pre style="word-wrap: break-word; white-space: pre-wrap;">
+    
+    
+    #Endpoints:
+    
+    /repo  
+    /repo?since=weekly
+    /repo/[language]
 
-    data = scraping.get_trending('', since)
+    /dev
+    /dev?since=weekly
+    /dev/[language]
 
-    if data:
-        return jsonify(data), 200
-    else:
-        return 404
-
-
-@main.route("/<language>")
-def get_language_trending(language):
-    since = request.args.get('since') if request.args.get(
-        'since') else 'daily'
-
-    data = scraping.get_trending(f"/{language}", since)
-
-    if data:
-        return jsonify(data), 200
-    else:
-        return 404
+    #Return Example: 
+    
+    /repo
+    {
+        "author": "public-apis",
+        "avatar_url":   "https://github.com/public-apis.png",
+        "description": "A collective list of free APIs",
+        "forks": 20506,
+        "full_name": "public-apis/public-apis",
+        "repo_name": "public-apis",
+        "repo_url": "https://github.com/public-apis/public-apis",
+        "stars": 178065
+    },
+    ...
+    
+    /dev
+    {
+        "rank_position": 1,
+            "name": PySimpleGUI,
+            "username": PySimpleGUI,
+            "dev_url": "https://github.com/PySimpleGUI",
+            "avatar": "https://github.com/PySimpleGUI.png",
+            "popularRepo": {
+                "repo_name": "PySimpleGUI",
+                "description": "Launched in 2018. It's 2022 and PySimpleGUI is actively developed & supported...",
+                "url": "https://github.com/PySimpleGUI/PySimpleGUI"
+            }
+    },
+    ...
+    </pre>
+    '''
